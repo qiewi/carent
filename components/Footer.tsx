f11a03819dd8b0406e511f32ade87dc80829e97e
qiewi <105619444+qiewi@users.noreply.github.com>
2024-09-29 11:57:47 +0700

    ft: footer finished

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
             className='object-contain'
           />
           <p className="text-base text-gray-700">
-            Carhub 2023 <br />
+            CarHub 2023 <br />
             All right reserved &copy;
           </p>
         </div>
@@ -37,7 +37,21 @@ const Footer = () => {
             </div>
           ))}
         </div>
-      </div>
+        </div>
+        
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+          <p>
+            @2023 CarHub. All Rights Reserved
+          </p>
+          <div className="footer__copyrights-link">
+            <Link href="/" className='text-gray-500'>
+              Privacy Policy
+            </Link>
+            <Link href="/" className='text-gray-500'>
+              Terms of Use
+            </Link>
+          </div>
+        </div>
     </footer>
   )
 }
